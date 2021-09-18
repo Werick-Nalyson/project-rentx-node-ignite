46c1703d823bca6513c5b3503f5f7e0251095b21
Werick Nalyson <wericknalyson@gmail.com>
2021-09-18 20:07:17 -0300

    refactor: create category repository for queries

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,25 +1,30 @@
 import { Request, Response, Router } from 'express';
 
-import { Category } from '../model/Category';
+import { CategoriesRepository } from '../repositories/CategoriesRepository';
 
 const categoriesRoutes = Router();
-
-const categories: Category[] = [];
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post('/', (request: Request, response: Response) => {
   const { name, description } = request.body;
 
-  const category = new Category();
+  const categoryAlreadyExists = categoriesRepository.findByName(name);
 
-  Object.assign(category, {
-    name,
-    description,
-    created_at: new Date(),
-  });
+  if (categoryAlreadyExists) {
+    return response.status(201).json({
+      error: 'Category Already exists!',
+    });
+  }
 
-  categories.push(category);
+  const category = categoriesRepository.create({ name, description });
 
   return response.status(201).json(category);
+});
+
+categoriesRoutes.get('/', (request: Request, response: Response) => {
+  const categories = categoriesRepository.list();
+
+  return response.json(categories);
 });
 
 export { categoriesRoutes };
