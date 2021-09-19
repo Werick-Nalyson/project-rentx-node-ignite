483e68f94a99703580e6e790641cce8bc7506866
Werick Nalyson <wericknalyson@gmail.com>
2021-09-19 11:07:51 -0300

    feat: add repository, service and model of specifications

---
+++ src/modules/cars/repositories/SpecificationsRepository.ts
@@ -0,0 +1,38 @@
+import { Category } from '../model/Category';
+import { Specification } from '../model/Specification';
+import {
+  ISpecificationsRepository,
+  ICreateSpecificationDTO,
+} from './ISpecificationsRepository';
+
+class SpecificationsRepository implements ISpecificationsRepository {
+  private specifications: Specification[];
+
+  constructor() {
+    this.specifications = [];
+  }
+
+  create({ name, description }: ICreateSpecificationDTO): Specification {
+    const specification = new Specification();
+
+    Object.assign(specification, {
+      name,
+      description,
+      created_at: new Date(),
+    });
+
+    this.specifications.push(specification);
+
+    return specification;
+  }
+
+  findByName(name: string): Category {
+    const category = this.specifications.find(
+      specification => specification.name === name,
+    );
+
+    return category;
+  }
+}
+
+export { SpecificationsRepository };
--- src/modules/cars/services/CreateSpecificationService.ts
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -1,6 +1,25 @@
+/* eslint-disable prettier/prettier */
+import { Specification } from '../model/Specification';
+import { ISpecificationsRepository } from '../repositories/ISpecificationsRepository';
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
 class CreateSpecificationService {
-  execute() {
-    console.log('TODO');
+  constructor(private specificationsRepository: ISpecificationsRepository) { }
+
+  execute({ name, description }: IRequest): Specification {
+    const categoryAlreadyExists = this.specificationsRepository.findByName(name);
+
+    if (categoryAlreadyExists) {
+      throw new Error('Specification Already exists!');
+    }
+
+    const specification = this.specificationsRepository.create({ name, description });
+
+    return specification;
   }
 }
 
