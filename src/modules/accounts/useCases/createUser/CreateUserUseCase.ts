18753e6880b6cefd06fd6e731e1b0bd2cdd84207
Werick Nalyson <wericknalyson@gmail.com>
2021-09-25 18:28:35 -0300

    feat: encrypting password

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,9 +1,11 @@
 /* eslint-disable prettier/prettier */
+import { hash } from 'bcrypt'
 import { inject, injectable } from 'tsyringe';
 
 import { ICreateUserDTO } from '../../dtos/ICreateUserDTO';
 import { User } from '../../entities/User';
 import { IUsersRepository } from '../../repositories/IUsersRepository';
+
 
 @injectable()
 class CreateUserUseCase {
@@ -18,11 +20,13 @@ class CreateUserUseCase {
     driver_license,
     password,
   }: ICreateUserDTO): Promise<User> {
+    const passwordHash = await hash(password, 8)
+
     const user = await this.usersRepository.create({
       name,
       email,
       driver_license,
-      password,
+      password: passwordHash,
     });
 
     return user;
