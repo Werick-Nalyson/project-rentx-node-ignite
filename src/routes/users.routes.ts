26ca1e1bbe6b2401df2bc3be0de44cdd8683e9c8
Werick Nalyson <wericknalyson@gmail.com>
2021-09-26 21:15:53 -0300

    feat: upload user avatar

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,11 +1,25 @@
 import { Router } from 'express';
+import multer from 'multer';
 
+import upload from '../config/upload';
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 import { CreateUserContoller } from '../modules/accounts/useCases/createUser/CreateUserContoller';
+import { UpdateUserAvatarController } from '../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController';
 
 const createUserController = new CreateUserContoller();
+const updateUserAvatarController = new UpdateUserAvatarController();
 
 const usersRoutes = Router();
 
+const uploadAvatar = multer(upload.upload('./tmp/avatar'));
+
 usersRoutes.post('/', createUserController.handle);
+
+usersRoutes.patch(
+  '/avatar',
+  ensureAuthenticated,
+  uploadAvatar.single('avatar'),
+  updateUserAvatarController.handle,
+);
 
 export { usersRoutes };
