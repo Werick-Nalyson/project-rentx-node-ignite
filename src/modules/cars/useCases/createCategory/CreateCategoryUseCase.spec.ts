b327b1355565c3d936943e000203bbf27000195d
Werick Nalyson <wericknalyson@gmail.com>
2021-09-29 22:09:52 -0300

    feat: add test of create category

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,39 @@
+import { AppError } from '../../../../errors/AppError';
+import { Category } from '../../entities/Category';
+import { CategoriesRepositoryInMemory } from '../../repositories/in-memory/CategoriesRepositoryInMemory';
+import { CreateCategoryUseCase } from './CreateCategoryUseCase';
+
+let createCategoryUseCase: CreateCategoryUseCase;
+let categoriesRepositoryInMemory: CategoriesRepositoryInMemory;
+
+describe('Create category', () => {
+  beforeEach(() => {
+    categoriesRepositoryInMemory = new CategoriesRepositoryInMemory();
+    createCategoryUseCase = new CreateCategoryUseCase(
+      categoriesRepositoryInMemory,
+    );
+  });
+
+  it('should be able to create a new category', async () => {
+    const category: Omit<Category, 'created_at'> = {
+      name: 'Category teste',
+      description: 'Description teste',
+    };
+
+    const categoryCreated = await createCategoryUseCase.execute(category);
+
+    expect(categoryCreated).toHaveProperty('id');
+  });
+
+  it('should not be able to create a new category if name already exists', async () => {
+    expect(async () => {
+      const category: Omit<Category, 'created_at'> = {
+        name: 'Category teste',
+        description: 'Description teste',
+      };
+
+      await createCategoryUseCase.execute(category);
+      await createCategoryUseCase.execute(category);
+    }).rejects.toBeInstanceOf(AppError);
+  });
+});
